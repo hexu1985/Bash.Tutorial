ee3e374f9912462e69e275cea59bab31665efb94
hexu <hexu_1985@sina.com>
2023-05-19 14:10:12 +0800

    add getting_started/recipe-03

---
+++ getting_started/recipe-03/code/str1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+your_name="runoob"
+str="Hello, I know you are \"$your_name\"! \n"
+echo -e $str
+
---
+++ getting_started/recipe-03/code/str3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+string="abcd"
+echo ${#string}   # 输出 4
+
+
+string="abcd"
+echo ${#string[0]}   # 输出 4
