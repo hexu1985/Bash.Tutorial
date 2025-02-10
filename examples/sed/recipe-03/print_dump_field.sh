a47d8801a92a9efca02aa2ee965c396885c7ec6d
xu.he <xu.he@phigent.ai>
2025-02-10 14:14:04 +0800

    add examples/sed/recipe-03

---
+++ examples/sed/recipe-03/print_dump_field.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+infile="test.txt"
+tmpfile=.${infile}.tmp
+outfile="output.txt"
+
+cp ${infile} ${tmpfile}
+
+sed -i 's/ptr_out->//g' ${tmpfile}
+sed -i 's/ =.*$//g' ${tmpfile}
+sed -i 's/^ *//g' ${tmpfile}
+
+rm -f ${outfile}
+cat ${tmpfile} | \
+while read line
+do
+    echo "    <<" '"'"    $line: "'"' "<<" "std::to_string(data.$line)" "<<" '"\n"' >> ${outfile}
+done
+
+
