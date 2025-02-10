142663fcea35c68e03f17ff9c5c256fcd717fc6f
xu.he <xu.he@phigent.ai>
2025-02-10 17:00:33 +0800

    update examples/sed/recipe-04

--- examples/sed/recipe-04/create_test_field.sh
+++ examples/sed/recipe-04/create_test_field.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ptr_name="local_pose_ptr"
+ptr_name="safety_function_output_ptr"
 infile="test.txt"
 tmpfile=.${infile}.tmp
 outfile="output.txt"
@@ -8,7 +8,7 @@ outfile="output.txt"
 cp ${infile} ${tmpfile}
 
 sed -i 's/^ *ptr_out->.*ptr_in->//g' ${tmpfile}
-sed -i "s/\(.*\);/    ${ptr_name}->\1 = 1;/g" ${tmpfile}
+sed -i "/\<for\>/!s/\(.*\);/    ${ptr_name}->\1 = 1;/g" ${tmpfile}
 
 mv ${tmpfile} ${outfile}
 
